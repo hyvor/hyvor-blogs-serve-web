c159cb665b0c3789c6bb590689d58b6f9d9ffef5
SupunKavinda <supunkavinda1125@gmail.com>
2023-07-22 06:38:47 +0200

    build

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,11 @@ export default defineConfig({
         root: 'tests',
         environment: 'happy-dom'
     },
+    build: {
+        lib: {
+            entry: 'src/index.ts',
+            name: 'hyvor-blogs-ts',
+            fileName: (format) => `hyvor-blogs-ts.${format}.js`
+        }
+    }
 })

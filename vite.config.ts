f9bfc9455adb07147762f17f4548d30814df0afa
SupunKavinda <supunkavinda1125@gmail.com>
2023-07-25 03:34:55 +0200

    build

--- vite.config.ts
+++ vite.config.ts
@@ -10,8 +10,8 @@ export default defineConfig({
     build: {
         lib: {
             entry: 'src/index.ts',
-            name: 'index',
-            fileName: (format) => `index.${format}.js`
+            name: '@hyvor/hyvor-blogs-serve-web',
+            fileName: 'index'
         }
     }
 })
